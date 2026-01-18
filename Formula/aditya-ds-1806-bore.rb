4a6b6fd5d5d2fb784273db9c773b25ade5988329
Aditya <adithya18062000@gmail.com>
2026-01-18 14:12:47 +0530

    add: bore formula v0.1.2

---
+++ Formula/aditya-ds-1806-bore.rb
@@ -0,0 +1,19 @@
+class AdityaDs1806Bore < Formula
+  desc "A blazing fast, free & open source TCP tunnel that exposes your localhost to the world."
+  homepage "https://trybore.com/"
+  url "https://github.com/Aditya-ds-1806/bore/releases/download/v0.1.2/bore-0.1.2.tar.gz"
+  sha256 "sha256:283d872f391f47073b3b7040d7e698572911d2b1c19ed44a3b18b0fc8582cddd"
+  license "MIT"
+
+  depends_on "make" => :build
+  depends_on "go" => :build
+
+  def install
+    system "make", "build-client"
+    bin.install "build/bore"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bore --version")
+  end
+end
