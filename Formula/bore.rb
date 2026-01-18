74fd68344527abea0697ef55cadb7cf17a7ac0d4
Aditya <adithya18062000@gmail.com>
2026-01-18 14:53:50 +0530

    fix: url and checksum

--- Formula/bore.rb
+++ Formula/bore.rb
@@ -1,8 +1,8 @@
 class Bore < Formula
   desc "Blazing fast, free TCP tunnel that exposes your localhost to the world"
   homepage "https://trybore.com/"
-  url "https://github.com/Aditya-ds-1806/bore/releases/download/v0.1.3/bore-0.1.3.tar.gz"
-  sha256 "a9c2c37447424d91b4729ee3d909fe8fbc385f82952ef81cc2194ea4e190af24"
+  url "https://github.com/Aditya-ds-1806/bore/releases/download/v0.1.4/bore-0.1.4.tar.gz"
+  sha256 "d544d9f2ae332bfcd25ee8be505894a23bd00af13b3dc3ab165a6f2c87911e8a"
   license "MIT"
 
   depends_on "go" => :build
