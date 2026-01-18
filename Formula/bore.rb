047f87a709a716ecb48131274b5e847abbfeb674
Aditya <adithya18062000@gmail.com>
2026-01-18 14:37:27 +0530

    fix: failing tests

--- Formula/bore.rb
+++ Formula/bore.rb
@@ -1,8 +1,8 @@
-class AdityaDs1806Bore < Formula
+class Bore < Formula
   desc "Blazing fast, free TCP tunnel that exposes your localhost to the world"
   homepage "https://trybore.com/"
-  url "https://github.com/Aditya-ds-1806/bore/releases/download/v0.1.2/bore-0.1.2.tar.gz"
-  sha256 "283d872f391f47073b3b7040d7e698572911d2b1c19ed44a3b18b0fc8582cddd"
+  url "https://github.com/Aditya-ds-1806/bore/releases/download/v0.1.3/bore-0.1.3.tar.gz"
+  sha256 "a9c2c37447424d91b4729ee3d909fe8fbc385f82952ef81cc2194ea4e190af24"
   license "MIT"
 
   depends_on "go" => :build
